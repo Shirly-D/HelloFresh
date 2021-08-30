4604a98bc43ad89c5a4e15ab9699f4fd5f4dbda0
Shirly <shirlyd@axioned.com>
2021-08-31 01:11:46 +0530

    js lightbox initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,10 @@ var slider = document.querySelector('.slider');
 if (slider) {
 		$('.slider').slick({    
 			dots: true,
+            autoplay:true,
             arrows:true,
-            prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-            nextArrow: '<button class="slide-arrow next-arrow"></button>',
+            prevArrow: '<button class="slide-arrow prev-arrow slick-arrow"></button>',
+            nextArrow: '<button class="slide-arrow next-arrow slick-arrow"></button>',
             slidesToShow: 3,
             slidesToScroll: 1
 		});
