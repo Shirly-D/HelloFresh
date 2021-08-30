ea8419baf57846f3c75a44d575fc752b57bfb74d
Shirly <shirlyd@axioned.com>
2021-08-31 00:33:36 +0530

    initial js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-/* Author: 
+// slider
 
-*/
+var slider = document.querySelector('.slider');
 
+if (slider) {
+		$('.slider').slick({    
+			dots: true,
+			autoplay: true,
+            arrows:true,
+            slidesToShow: 3,
+            slidesToScroll: 1
+		});
+}
 
 
 
