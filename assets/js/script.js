9aa09306aab8afc2f625c2a4ccf4e29fd6a83587
Shirly <shirlyd@axioned.com>
2021-08-31 01:11:55 +0530

    js lightbox initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,16 @@ if (slider) {
 		});
 }
 
+// lightbox
+
+var box = document.querySelectorAll('.content-box');
+for (var i = 0; i < box.length; i++) {
+    box[i].addEventListener('click', function(e){
+        var boxElem = e.currentTarget.getAttribute('data-image');
+        var text = e.currentTarget.getAttribute('data-text');
+        lightBox(boxElem, text);
+    })
+}
 
 
 
