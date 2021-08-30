f72b8944d8d651fdaf09c9baebd16b2803930e28
Shirly <shirlyd@axioned.com>
2021-08-31 00:49:15 +0530

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@ var slider = document.querySelector('.slider');
 if (slider) {
 		$('.slider').slick({    
 			dots: true,
-			autoplay: true,
             arrows:true,
+            prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+            nextArrow: '<button class="slide-arrow next-arrow"></button>',
             slidesToShow: 3,
             slidesToScroll: 1
 		});
